b21e56943d2c3b794b45bafcba3193bc4f7f5f03
CaiqueSmd <36681822+CaiqueSmd@users.noreply.github.com>
2018-09-17 00:05:39 -0300

    Mudanças no html, css e ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,29 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
+  //função para mostrar e esconder a busca avançada.
+  mostrar(){
+    var valor:boolean;
+    valor = $(".dropdown-content").hasClass("show");
+    if (valor == true) {
+      $(".dropdown-content").removeClass("show");
+      console.log(valor);
+    }
+    else{
+      $(".dropdown-content").addClass("show");
+    }
+  }
 
+  habilitar(){
+    if (valores == true){
+      $(".item").css({"disabled":"false"});
+    }
+    else{
+      $(".item").css({"disabled":"true"});
+    }
+    
   }
 
 }
